264b4a3997a45e3e91c2c022297ad658bd1a3adc
lwt1995 <1152925359@qq.com>
2022-06-05 15:16:48 +0800

    igitnore

--- threadWirteFile.cpp
+++ threadWirteFile.cpp
@@ -7,21 +7,35 @@ void wirteFile(FILE *f)
 {
     for (size_t i =100; i < 500; i++)
     {
-        fprintf(f,"func %s(), current time %s, index %d, threadId:%d\n", __func__, __TIME__, i, this_thread::get_id()); 
+        fprintf(f,"func: %s(), line: %d  current time: %s, index: %d, threadId:%d\n",
+            __func__, __LINE__, __TIME__, i, this_thread::get_id()); 
     }
 }
 
 void syncWirteFile(FILE *f, int index)
 {
+    
     for (size_t i = 0; i < index; i++)
     {
        std::async(launch::async, [&]() {  
             for (size_t j = i * 20; j < (i + 1) * 100 ; j++)
             {
-                fprintf(f,"func %s(), current time %s, index %d, threadId:%d\n", __func__, __TIME__,
+                fprintf(f,"func: %s(),line: %d, current time: %s, index: %d, threadId:%d\n",
+                    __func__, __LINE__, __TIME__,
                     j, this_thread::get_id()); 
             }
        });
+    }
+}
+
+void readFileContent()
+{
+    FILE *f = fopen("d://a.txt", "r+");
+     char s[1024] = { 0 };   
+    while(fgets(s, sizeof(s), f) != nullptr) {
+        printf("%s\n",s);
+    }
+    fclose(f);
 }
 int main() {
     
@@ -31,10 +45,9 @@ int main() {
        printf("file open error");
        return 0;
     }
-    syncWirteFile(f, 5);
-    
-    //wirteFile(f);
+    // syncWirteFile(f, 5);
+    wirteFile(f); 
     fclose(f);
-    
+    readFileContent();
     return 0;
 }
